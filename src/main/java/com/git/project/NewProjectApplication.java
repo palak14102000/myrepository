f1fb18c3eff8123eb065dff42c0a7916e5bccc64
palak14102000 <diwakarpalak20@gmail.com>
2024-02-12 10:01:44 +0530

    new project

---
+++ src/main/java/com/git/project/NewProjectApplication.java
@@ -0,0 +1,14 @@
+package com.git.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewProjectApplication.class, args);
+		System.out.println("Hello there");
+	}
+
+}
